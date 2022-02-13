f092779f5f4892ae99365fdc33b0ddb472d307f9
yui-nagamatsu <yuinm129@gmail.com>
2022-02-13 22:33:09 +0900

    rewrite DATABASES in setting

--- app/src/yuismedia/settings.py
+++ app/src/yuismedia/settings.py
@@ -84,8 +84,18 @@ WSGI_APPLICATION = 'yuismedia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite',
+        'USER': 'mysiteuser',
+        'PASSWORD': '',
+        'HOST': 'db',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
+        'TEST': {
+            'NAME': 'test_database',
+        },
     }
 }
 
