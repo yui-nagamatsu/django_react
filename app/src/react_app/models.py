2ede858b9d88dc09ef43a751575867046656435d
yui-nagamatsu <yuinm129@gmail.com>
2022-02-14 12:27:38 +0900

    Introduction of test data

--- app/src/react_app/models.py
+++ app/src/react_app/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+class User_Models(models.Model):
+    
+    class Meta:
+        db_table = 'User'
+
+    user_id = models.CharField(
+        verbose_name='userid',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+
+    user_name = models.CharField(
+        verbose_name='username',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+
+    user_age = models.CharField(
+        verbose_name='userage',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+
+    user_email = models.CharField(
+        verbose_name='useremail',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+
+    def __str__(self):
+        return self.user_id
