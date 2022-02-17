83c89f93970f50a9c601f2b85598b71b741492ca
yui-nagamatsu <yuinm129@gmail.com>
2022-02-17 12:43:10 +0900

    Set the class named Pokemon_Models. Reference is pokedex-from-spreadsheet.json.

--- app/src/react_app/models.py
+++ app/src/react_app/models.py
@@ -1,37 +1,201 @@
+from math import atanh
 from django.db import models
 
 # Create your models here.
-class User_Models(models.Model):
+
+# perとnatはよくわからないので、numberで一括りにした。
+# "LK/GK", "EV Worth", "EXPV"は不明なので、無視。
+class Pokemon_Models(models.Model):
+    
     
     class Meta:
-        db_table = 'User'
+        db_table = 'Pokemon'
 
-    user_id = models.CharField(
-        verbose_name='userid',
+    pokemon_number = models.CharField(
+        verbose_name='Number',
         blank=True,
         null=True,
         max_length=225,
         default='',
     )
 
-    user_name = models.CharField(
-        verbose_name='username',
+    pokemon_name = models.CharField(
+        verbose_name='Name',
         blank=True,
         null=True,
         max_length=225,
         default='',
     )
 
-    user_age = models.CharField(
-        verbose_name='userage',
+    hp = models.CharField(
+        verbose_name='HP',
         blank=True,
         null=True,
         max_length=225,
         default='',
     )
 
-    user_email = models.CharField(
-        verbose_name='useremail',
+    atk = models.CharField(
+        verbose_name='Attack',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+
+    dfs = models.CharField(
+        verbose_name='Defense',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+
+    spatk = models.CharField(
+        verbose_name='Special Attack',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+
+    spdfs = models.CharField(
+        verbose_name='Special Defense',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+
+    spe = models.CharField(
+        verbose_name='Speed',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+
+    total = models.CharField(
+        verbose_name='Total',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+
+    type_1 = models.CharField(
+        verbose_name='Type I',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+
+    type_2 = models.CharField(
+        verbose_name='Type II',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+
+    tier = models.CharField(
+        verbose_name='Tier',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+
+    ability_1 = models.CharField(
+        verbose_name='Ability I',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+
+    ability_2 = models.CharField(
+        verbose_name='Ability II',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+
+    hidden_ability = models.CharField(
+        verbose_name='Hidden Ability',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+
+    weight = models.CharField(
+        verbose_name='Weight',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+
+    color = models.CharField(
+        verbose_name='Color',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+
+    hatch = models.CharField(
+        verbose_name='Hatch',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+
+    gender = models.CharField(
+        verbose_name='Gender',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+
+    egg_group_1 = models.CharField(
+        verbose_name='Egg Group I',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+    egg_group_2 = models.CharField(
+        verbose_name='Egg Group II',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+
+    catch = models.CharField(
+        verbose_name='Catch',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+
+    exp = models.CharField(
+        verbose_name='EXP',
+        blank=True,
+        null=True,
+        max_length=225,
+        default='',
+    )
+
+    evolve = models.CharField(
+        verbose_name='Evolve',
         blank=True,
         null=True,
         max_length=225,
@@ -39,4 +203,4 @@ class User_Models(models.Model):
     )
 
     def __str__(self):
-        return self.user_id 
+        return self.pokemon_number
