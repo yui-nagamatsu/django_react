4df8cd21847a363c0e3eba16e780b5210f636160
yui-nagamatsu <yuinm129@gmail.com>
2022-02-20 09:40:51 +0900

    change the name of model User_Models to Pokemon

--- app/src/api/views.py
+++ app/src/api/views.py
@@ -1,14 +1,14 @@
 from rest_framework import viewsets, routers
-from react_app.models import User_Models
+from react_app.models import Pokemon
 from .serializers import UserSerializer
 
 
 class UserApi(viewsets.ModelViewSet):
-    queryset = User_Models.objects.all()
+    queryset = Pokemon.objects.all()
     serializer_class = UserSerializer
 
     def get_queryset(self):
-        queryset = User_Models.objects.all()
+        queryset = Pokemon.objects.all()
         L_id = self.request.query_params.get('id')
 
         if L_id:
