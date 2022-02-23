0b231a9d2141afbc38a4ca67e091da730a73557c
yui-nagamatsu <yuinm129@gmail.com>
2022-02-23 16:46:33 +0900

    Correct the class DetailView.
    class DetailView(generic.DetailView):
        model = Pokemon
        template_name = 'backend/detail.html'
        def get_object(self, queryset=None):
            obj = super().get_object(queryset=queryset)
            return obj
    At the moment, the code above is fine.
    
    class DetailView(generic.DetailView):
        model = Pokemon
        template_name = 'backend/detail.html'
        def get_object(self):
            return Pokemon.objects.get('__all__')
    Sending the code above, it will throw the error below.
    ValueError: too many values to unpack (expected 2)

--- app/src/backend/views/backend.py
+++ app/src/backend/views/backend.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the backend index.")
+from backend.models import Pokemon
+
+class IndexView(generic.ListView):
+    template_name = 'backend/index.html'
+    context_object_name = 'random_featured_pokemon'
+    def get_queryset(self):
+        return Pokemon.objects.order_by('?')[:10]
+
+#class DetailView(generic.DetailView):
+#    model = Pokemon
+#    template_name = 'backend/detail.html'
+#    def get_object(self):
+#        return Pokemon.objects.get('__all__')
+
+
+class DetailView(generic.DetailView):
+    model = Pokemon
+    template_name = 'backend/detail.html'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset=queryset)
+        return obj
