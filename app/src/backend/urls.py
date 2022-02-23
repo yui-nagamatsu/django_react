e47da7d334d20e1ac601421a17f606ce89798b8e
yui-nagamatsu <yuinm129@gmail.com>
2022-02-23 16:45:36 +0900

    Correct the path to detail page

--- app/src/backend/urls.py
+++ app/src/backend/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from .views import backend
+from django.urls import reverse
 
+
+from .views import backend
+
+
+app_name = 'backend'
 urlpatterns =[
-    path('', backend.index, name="index"),
+    path('', backend.IndexView.as_view(), name = 'index'),
+    path('<int:pk>/', backend.DetailView.as_view(), name='detail'),
 ]
+
