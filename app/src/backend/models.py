c15af1083629a065e67bd75af58b1555026d5606
yui-nagamatsu <yuinm129@gmail.com>
2022-03-10 17:14:09 +0900

    Adopt ForeignKey about pokemon name and their types. Use ForeignKey instead of ManyToManyField.

--- app/src/backend/models.py
+++ app/src/backend/models.py
@@ -2,6 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+class TypeName(models.Model):
+    
+    class Meta:
+        db_table = 'typename'
+
+    type_name =  models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.type_name 
+
+
 class Pokemon(models.Model):    
     
     class Meta:
@@ -9,11 +20,9 @@ class Pokemon(models.Model):
 
     number = models.IntegerField(verbose_name="Number")
 
-    name = models.CharField(verbose_name="Name", max_length=25)
+    pokemon_name = models.CharField(verbose_name="Name", max_length=25)
 
-    type1 = models.CharField(verbose_name="Type1", max_length=25)
-
-    type2 = models.CharField(verbose_name="Type2", max_length=25, default='')
+    # type_name = models.ForeignKey("TypeName", on_delete=models.PROTECT)
 
     heart_point = models.IntegerField(verbose_name="HP")
 
@@ -33,3 +42,16 @@ class Pokemon(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class PokemonType(models.Model):
+
+    class Meta:
+        db_table = 'pokemon_type'
+
+    pokemon_name = models.ForeignKey("Pokemon", on_delete=models.PROTECT)
+
+    type_name = models.ForeignKey("TypeName", on_delete=models.PROTECT)
+
+    def __str__(self) -> str:
+        return self.pokemon.pokemon_name + ":" + self.typename.type_name
